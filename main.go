a0e7f6ab7d38f07199db989f87d8f064ff6e0e06
hclabxing <316088973@qq.com>
2016-10-01 00:02:52 +0800

    参数

--- main.go
+++ main.go
@@ -7,10 +7,14 @@ import (
 func main() {
 	fmt.Println("hello word!你好，世界！")
 
-	// var variableName int = 1 s
-	var variableName = 1
-	var vname1, vname2, vname3 int
-
-	fmt.Println(variableName)
-	fmt.Println(vname1 + vname2 + vname3)
 }
+
+const PI = 3.14
+
+var name = "gopher"
+
+type newType int
+
+type gopher struct{}
+
+type golang interface{}
