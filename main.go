f27b659c7ea310e9a02c7876406be720cc279875
hclabxing <316088973@qq.com>
2016-09-30 17:54:28 +0800

    添加答应

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 func main() {
 	fmt.Println("hello word!你好，世界！")
 
-	var variableName int
+	// var variableName int = 1
+	var variableName = 1
 	var vname1, vname2, vname3 int
 
+	fmt.Println(variableName)
+	fmt.Println(vname1 + vname2 + vname3)
 }
